9200a1a4cbd2861fa67a1a6b51dcdf9a59d65bc7
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-15 19:19:34 +0000

    update step state

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,43 @@
-import React, { useState } from 'react';
-import Sequencer from 'comps/seq';
+import { useState } from 'react';
 
-type State = { steps: number[]; currentStep: number; playing: boolean };
+type Step = { id: number; on: 1 | 0 };
+
+type State = { steps: Step[]; currentStep: number; playing: boolean };
 
 const App = () => {
   const [state, setState] = useState<State>({
-    steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    steps: [
+      { id: 1, on: 0 },
+      { id: 2, on: 0 },
+      { id: 3, on: 0 },
+      { id: 4, on: 0 },
+      { id: 5, on: 0 },
+      { id: 6, on: 0 },
+      { id: 7, on: 0 },
+      { id: 8, on: 0 },
+      { id: 9, on: 0 },
+      { id: 10, on: 0 },
+      { id: 11, on: 0 },
+      { id: 12, on: 0 },
+      { id: 13, on: 0 },
+      { id: 14, on: 0 },
+      { id: 15, on: 0 },
+      { id: 16, on: 0 },
+    ],
     currentStep: 0,
     playing: false,
   });
 
   return (
     <div>
-      <Sequencer steps={state.steps} />
+      {state.steps.map(({ id, on }) => (
+        <div
+          key={id}
+          style={{ width: 10, height: 10, border: '1px solid blue' }}
+        >
+          {id}
+        </div>
+      ))}
     </div>
   );
 };
--- src/comps/seq.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Sequencer = ({ steps }: any) => {
-  return <div>Sequencer</div>;
-};
-
-export default Sequencer;
