209a148dc8f9ac953598f6f5b987617143db6f6e
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-16 11:22:17 +0000

    add elem audio

--- src/App.tsx
+++ src/App.tsx
@@ -1,109 +1,24 @@
 /** @jsxImportSource @emotion/react */
 import { useState, useEffect } from 'react';
 import { StepListUL, Step, CircleButton } from 'comps/lib';
-import WAAClock from 'waaclock';
 
-type Step = { id: number; on: 1 | 0 };
+type Step = 1 | 0;
 
 type State = { steps: Step[]; currentStep: number; isPlaying: boolean };
 
 const App = () => {
   const [state, setState] = useState<State>({
-    steps: [
-      { id: 0, on: 0 },
-      { id: 1, on: 0 },
-      { id: 2, on: 0 },
-      { id: 3, on: 0 },
-      { id: 4, on: 0 },
-      { id: 5, on: 0 },
-      { id: 6, on: 0 },
-      { id: 7, on: 0 },
-      { id: 8, on: 0 },
-      { id: 9, on: 0 },
-      { id: 10, on: 0 },
-      { id: 11, on: 0 },
-      { id: 12, on: 0 },
-      { id: 13, on: 0 },
-      { id: 14, on: 0 },
-      { id: 15, on: 0 },
-    ],
+    steps: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     currentStep: 0,
     isPlaying: false,
   });
 
-  const setStep = (id: number) => {
+  const setStep = (index: number) => {
     const copySteps = [...state.steps];
 
-    const copyObj = { ...copySteps[id] };
-
-    if (copyObj.on === 0) {
-      copyObj.on = 1;
-    } else {
-      copyObj.on = 0;
-    }
-
-    copySteps[id] = copyObj;
+    copySteps[index] = state.steps[index] === 1 ? 0 : 1;
 
     setState((prev) => ({ ...prev, steps: copySteps }));
-  };
-
-  const [audioCtx, setAudioCtx] = useState<AudioContext>();
-  let clock: WAAClock;
-  let tickEvent: WAAClock.Event | null;
-
-  useEffect(() => {
-    console.log(audioCtx);
-    if (audioCtx) {
-      clock = new WAAClock(audioCtx);
-    }
-  }, [audioCtx]);
-
-  const handlePlay = () => {
-    if (!state.isPlaying) {
-      setState((prev) => ({ ...prev, currentStep: -1, isPlaying: true }));
-    }
-  };
-
-  useEffect(() => {
-    if (!audioCtx || !clock) return;
-    if (state.isPlaying) {
-      clock.start();
-      tickEvent = clock
-        .callbackAtTime(handleTick, audioCtx.currentTime)
-        .repeat(0.47);
-    } else {
-      clock.stop();
-      tickEvent?.clear();
-      tickEvent = null;
-    }
-  }, [state.isPlaying]);
-
-  const handleTick = ({ deadline }: { deadline: number }) => {
-    const { currentStep, steps } = state;
-    const newCurrentStep = currentStep + 1;
-
-    if (steps[newCurrentStep % steps.length] && audioCtx) {
-      triggerSound(audioCtx, deadline);
-    }
-
-    setState((prev) => ({ ...prev, currentStep: newCurrentStep }));
-  };
-
-  const triggerSound = (audioCtx: AudioContext, deadline: number) => {
-    const oscillator = audioCtx.createOscillator();
-    const amplifier = audioCtx.createGain();
-
-    oscillator.frequency.setValueAtTime(200, deadline);
-    oscillator.frequency.linearRampToValueAtTime(50, deadline + 0.15);
-    oscillator.start(deadline);
-
-    oscillator.connect(amplifier);
-    amplifier.gain.setValueAtTime(0, deadline);
-
-    amplifier.gain.linearRampToValueAtTime(1.0, deadline + 0.02);
-    amplifier.gain.linearRampToValueAtTime(0.0, deadline + 0.2);
-
-    amplifier.connect(audioCtx.destination);
   };
 
   return (
@@ -117,12 +32,13 @@ const App = () => {
         maxWidth: '840px',
       }}
     >
-      <CircleButton onClick={() => handlePlay()} />
-      <CircleButton onClick={() => setAudioCtx(new AudioContext())} />
       <StepListUL>
-        {state.steps.map(({ id, on }) => (
-          <li key={id}>
-            <Step variant={on ? 'on' : 'off'} onClick={() => setStep(id)} />
+        {state.steps.map((step, index) => (
+          <li key={index}>
+            <Step
+              variant={step === 1 ? 'on' : 'off'}
+              onClick={() => setStep(index)}
+            />
           </li>
         ))}
       </StepListUL>
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { el } from '@elemaudio/core';
+import WebRenderer from '@elemaudio/web-renderer';
 import 'styles/global.css';
+
+const ctx = new AudioContext();
+const core = new WebRenderer();
+
+core.on('load', function () {
+  core.render(el.cycle(440), el.cycle(441));
+});
+
+(async function main() {
+  let node = await core.initialize(ctx, {
+    numberOfInputs: 0,
+    numberOfOutputs: 1,
+    outputChannelCount: [2],
+  });
+
+  node.connect(ctx.destination);
+})();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
