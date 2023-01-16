67f667ac1fa1155dc9c28c57b33efc09a1c52f96
Alex Gilbert <alex.js.ltd@gmail.com>
2023-01-16 00:21:22 +0000

    update grid

--- src/comps/lib.tsx
+++ src/comps/lib.tsx
@@ -1,18 +1,26 @@
 /** @jsxImportSource @emotion/react */
 import * as colors from 'styles/colors';
-
+import * as mq from 'styles/media-queries';
 import styled from '@emotion/styled';
 import { keyframes, CSSObject } from '@emotion/react';
 
 const StepListUL = styled.ul({
+  padding: '1em',
   maxWidth: '840px',
   width: '100%',
   margin: 'auto',
   listStyle: 'none',
-  padding: '0',
   display: 'grid',
   gridTemplateColumns: `repeat(16, 1fr)`,
   gridGap: '1em',
+
+  [mq.small]: {
+    gridTemplateColumns: `repeat(8, 1fr)`,
+  },
+
+  [mq.mobile]: {
+    gridTemplateColumns: `repeat(4, 1fr)`,
+  },
 });
 
 type StepVariant = { on: CSSObject; off: CSSObject };
